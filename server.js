512e44aa95d97d8732310195e379ad09754830cc
danielLeiteSilva <daniel199257@gmail.com>
2024-02-05 00:20:49 -0300

    Add new mongo

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const { getDada } = require("./automation")
 const redis = require("redis")
 const fs = require("fs")
 const app = express()
+client_mongo = new MongoClient(process.env.MONGO_URI || '')
+client_mongo.db(process.env.DB_NAME)
 
 const swaggerUi = require('swagger-ui-express');
 
@@ -25,15 +27,18 @@ app.get("/api/v1", (req, res) => {
 
 app.get("/api/v1/alagamentos", async (req, res) => {
 
-  const client = await redis.createClient({url: "redis://0.0.0.0:6379"})
+  const client_redis = await redis.createClient({url: "redis://0.0.0.0:6379"})
   .on('error', err => console.log('Redis Client Error', err))
   .connect();
+
+  const db = await client_mongo.db(process.env.DB_NAME)
+  const connection = await db.collection(process.env.COLLECTION)
 
   const data = req.body.data
 
   let result = {}
 
-  const getinfo = await client.get(data)
+  const getinfo = await client_redis.get(data)
   if(!getinfo){
     result = await getDada(data)
     await client.set(data, JSON.stringify(result))
