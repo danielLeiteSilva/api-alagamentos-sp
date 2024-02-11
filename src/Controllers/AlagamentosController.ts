73911f29870d938f1f8a07fb4e5bf821030a2480
Daniel Silva <daniel199257@gmail.com>
2024-02-11 12:41:29 -0300

    Create new file from to deploy in vercel and new route for get all locals (#5)
    * Convert first project write in js to typescript and add design of solution (#1)
    
    * Add typescript
    
    * Create instance
    
    * Add
    
    * Add
    
    * Change
    
    * Add new config
    
    * Editing
    
    ---------
    
    Co-authored-by: Replit user <>
    
    * Add new strategy to scrapping data that before is maked using puppeteer (#2)
    
    * Add typescript
    
    * Create instance
    
    * Add
    
    * Add
    
    * Change
    
    * Add new config
    
    * Editing
    
    * Add
    
    * Add
    
    * Add crapping
    
    * Add crapping
    
    * Finishing
    
    ---------
    
    Co-authored-by: Replit user <>
    
    * Add
    
    * add new range between data (#4)
    
    * Add vercel json
    
    * add

--- src/Controllers/AlagamentosController.ts
+++ src/Controllers/AlagamentosController.ts
@@ -1,51 +1,28 @@
 import MongoClient from "../Repositorys/Mongo/MongoClient";
 import RedisClient from "../Repositorys/Redis/RedisClient";
-import NavigationService from "../Services/PuppeteerService/NavigationService";
-import Utils from "../Utils/Utils";
+import Strategy from "../Strategy/Strategy";
 
 class AlagamentosController {
   private mongoClient: MongoClient;
   private redisClient: RedisClient;
-  private navigationService: NavigationService;
+  private strategy: Strategy
 
   constructor() {
     this.mongoClient = new MongoClient();
     this.redisClient = new RedisClient();
-    this.navigationService = new NavigationService();
+    this.strategy = new Strategy();
   }
 
-
-   execute = async (req: any, res: any) => {
-    const body = req.body.data;
+  execute = async (req: any, res: any) => {
+    const date = req.body.data;
     try {
       await Promise.all([
         this.redisClient.connect(),
         this.mongoClient.connect()
       ])
-      const findRedis = await this.redisClient.get(body);
-      if (!findRedis) {
-        const findMongo = await this.mongoClient.find({
-          date: Utils.formatData(body),
-        });
-        if (!findMongo) {
-          const result = await this.navigationService.run(body);
-          await this.mongoClient.add(result);
-          await this.redisClient.set(body, JSON.stringify(result));
-          res.status(200).json(result);
-        } else {
-          await this.redisClient.set(body, JSON.stringify(findMongo));
-          res.status(200).json(findMongo);
-        }
-      } else {
-        const findMongo = await this.mongoClient.find({
-          date: Utils.formatData(body),
-        });
-        if (!findMongo) {
-          const { _id, ...data } = JSON.parse(findRedis);
-          await this.mongoClient.add(data);
-        }
-        res.status(200).json(JSON.parse(findRedis));
-      }
+      const result: any = await this.strategy.run(this.redisClient, this.mongoClient, date)
+      res.status(200).json(result);
+      
     } catch (error: any) {
       res
         .status(400)
--- src/Router.ts
+++ src/Router.ts
@@ -1,11 +1,12 @@
 import express, { Router } from "express";
 import Alive from "./Controllers/Alive";
 import AlagamentosController from "./Controllers/AlagamentosController";
+import AlagamentosPeriodoController from "./Controllers/AlagamentosPeriodoController";
 
 const router: Router = express.Router();
 
 router.get("/api/v1", Alive);
 router.post("/api/v1/alagamentos/data", AlagamentosController.execute);
-router.post("/api/v1/alagamentos/periodo", AlagamentosController.execute);
+router.post("/api/v1/alagamentos/periodo", AlagamentosPeriodoController.execute);
 
 export default router;
