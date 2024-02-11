7be4565b97e651096837a3a9987673778b522035
danielLeiteSilva <daniel199257@gmail.com>
2024-02-11 13:26:16 -0300

    add

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-import router from "./src/Router";
+import router from "./Router";
 import express, { Express } from "express";
 import swaggerUi from "swagger-ui-express";
 
@@ -8,7 +8,7 @@ const app: Express = express();
 
 const port = process.env.PORT || 8080
 
-import SwaggerService from "./src/Services/Swagger/SwaggerService";
+import SwaggerService from "./Services/Swagger/SwaggerService";
 
 //Use
 app.use('/api/v1/swagger-ui', swaggerUi.serve, swaggerUi.setup(SwaggerService.toJson()));
