09d0c14dde5f20436eb537c35747f7633e394b53
danielLeiteSilva <daniel199257@gmail.com>
2024-07-20 02:28:07 -0300

    Add

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,10 @@ config();
 
 import router from "./Router";
 import express, { Express } from "express";
-import swaggerUi from "swagger-ui-express";
 
 const app: Express = express();
 
 const port = process.env.PORT || 8080
-
-import SwaggerService from "./Services/Swagger/SwaggerService";
-
-//Use
-app.use('/api/v1/swagger-ui', swaggerUi.serve, swaggerUi.setup(SwaggerService.toJson()));
 
 app.use(express.json());
 app.use(router);
